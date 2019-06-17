cd87d411dfa38117acd8ab5fa2edc35c2b9b2c8b
Slav Donchev <slav.donchev@gmail.com>
2019-06-17 15:27:32 +0300

    add-ex8.05

---
+++ ch08/ex8.05-conv-to-conc/main.go
@@ -0,0 +1,76 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 58.
+//!+
+
+// Surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	var wg sync.WaitGroup
+	pp := make(chan string)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			wg.Add(1)
+			go func(i, j int) {
+				defer wg.Done()
+				ax, ay := corner(i+1, j)
+				bx, by := corner(i, j)
+				cx, cy := corner(i, j+1)
+				dx, dy := corner(i+1, j+1)
+				pp <- fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}(i, j)
+		}
+	}
+	go func() {
+		wg.Wait()
+		close(pp)
+	}()
+	for p := range pp {
+		fmt.Println(p)
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+//!-
